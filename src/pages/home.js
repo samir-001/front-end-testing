cbfda0ca688915cde68ba4d3f27feb41c3de4be3
samir-001 <samir.harg@yahoo.com>
2023-07-18 20:05:32 +0300

    edit article card

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import { useDispatch ,useSelector} from 'react-redux';
 import { getArticles } from '../reducers/articles/getArticles';
 import { Link } from 'react-router-dom';
 import {data} from "../data"
+import  { intervalToDuration ,format, parseISO} from "date-fns"
 const Home = ()=>{
 //   const dispatch = useDispatch()
 
@@ -14,6 +15,18 @@ const Home = ()=>{
 //   return state.Articles.data
 // })
 
+function getDurationSrting(date = ""){
+  const interval = intervalToDuration({
+    start: new Date("2023-07-18T14:21:00.800Z"),
+    end : new Date()
+  })
+   const sec = interval.seconds > 0 ?  interval.seconds + " " + "ثانية" : ""
+   const min = interval.minutes > 0 ?  interval.minutes + " " +"دقيقة"  : ""
+   const hr = interval.hours > 0 ?   interval.hours + " " + "ساعة": ""
+   const dy = interval.days > 0 ? interval.days +" " + "يوم"  : ""
+   const yr = interval.years > 0 ?  interval.years + " " + "عام" : ""
+  return sec + " " + min + " " + hr + " " + dy + " " + yr
+}
 var article ;
 if(data){
   article = data.map((item,index )=>{
@@ -21,14 +34,19 @@ if(data){
       
       <div key={index} className='summary-card'>
         <Link to={`/article/${item.Aid}`} state={{data:item}}>
-        <div className='summary-card__title'>{item.title}</div>
+        <div className='summary-card__title'>{item.title  +"[اخر تحديث]"+ new Date("2023-07-18T14:21:00.800Z").toLocaleDateString() }</div>
         <div className='summary-card__content'>{String(item.content).slice(0,30)}</div>
       </Link>
-        <div className='summary-card__like'></div>
+        <div className='summary-card__meta'>
+          <div> عدد الزوار : 0</div>
+          <div> منذ : {getDurationSrting()} </div>
+        </div>
       </div>
     )
   })
 } 
+
+
 
 return(  <div>
  <div className='jumbotron'>
@@ -50,6 +68,7 @@ return(  <div>
     </div>
 )
 }
+
   
 
 export default Home
